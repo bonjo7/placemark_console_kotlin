65467e9cb11225e6689a3856ae07decde258ce7d
Bernard Thompson <bethomps@redhat.com>
2019-09-19 15:11:50 +0100

    [Lab1] Added logging support

--- src/main/kotlin/org.wit.placemark.console.main.kt
+++ src/main/kotlin/org.wit.placemark.console.main.kt
@@ -1,3 +1,8 @@
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
 fun main(args: Array<String>){
+    logger.info { "Launching Placemark Console App" }
     println("Placemark Kotlin App Version 1.0")
 }
